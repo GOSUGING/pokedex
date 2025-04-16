b5a28452254d219dd03af31dd38c12e7bce054b0
Aron Suy <157416423+GOSUGING@users.noreply.github.com>
2025-04-15 20:06:40 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,7 +47,7 @@ $(document).ready(function(){
                     name: data.name,
                     height: data.height,
                     weight: data.weight,
-                    base_experience: data.base_experience,
+                    basexperience: data.base_experience,
                     types: data.types,
                     stats: data.stats // Array de estadísticas del Pokémon
                 };
